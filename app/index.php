4b4c0663a98dada35a952642867345a3c6f35587
Wilhelmus <wilhelm_andrean_22@yahoo.com>
2016-04-07 11:38:02 +0700

    dummy database

--- app/index.php
+++ app/index.php
@@ -9,11 +9,8 @@
   <link rel="stylesheet" href="../libs/assets/font-awesome/css/font-awesome.min.css" type="text/css" />
   <link rel="stylesheet" href="../libs/assets/simple-line-icons/css/simple-line-icons.css" type="text/css" />
   <link rel="stylesheet" href="../libs/jquery/bootstrap/dist/css/bootstrap.css" type="text/css" />
-
   <link rel="stylesheet" href="css/font.css" type="text/css" />
   <link rel="stylesheet" href="css/style.css" type="text/css" />
-  
-
 </head>
 <body>
 <div class="app app-header-fixed ">
